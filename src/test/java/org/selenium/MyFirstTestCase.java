f99c2167ef5048e8bad573ad59603dbab1cd9d15
Sergio Villagomez Ortega <martin.v.ortega@gmail.com>
2023-04-11 21:43:16 -0500

    Primer Test de prueba Success

---
+++ src/test/java/org/selenium/MyFirstTestCase.java
@@ -0,0 +1,32 @@
+package org.selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+// import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
+
+public class MyFirstTestCase {
+
+    @Test
+    public void dumyTest(){
+
+        // ChromeOptions chromeOptions = new ChromeOptions();
+        // chromeOptions.addArguments("--remote-allow-origins=*");
+
+        // System Property for Chrome Driver
+        System.setProperty("webdriver.chrome.driver","D:\\Drivers\\chromedriver_111.0.5563.41.exe");
+        // Instantiate a ChromeDriver class.
+        WebDriver driver=new ChromeDriver();
+
+        // Launch Website
+        driver.get("https://askomdch.com/");
+
+        //Maximize the browser
+        driver.manage().window().maximize();
+
+        driver.close();
+        driver.quit();
+
+    }
+
+}
