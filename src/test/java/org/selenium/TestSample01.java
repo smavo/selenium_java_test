4cbabcfd0ff3b1ef1aefdc58b18847b11e354768
Sergio Villagomez Ortega <martin.v.ortega@gmail.com>
2023-04-12 16:47:55 -0500

    Agregando primer Test Automatizado de ejemplo

--- src/test/java/org/selenium/TestSample01.java
+++ src/test/java/org/selenium/TestSample01.java
@@ -2,16 +2,12 @@ package org.selenium;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-// import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
 
-public class MyFirstTestCase {
+public class TestSample01 {
 
     @Test
     public void dumyTest(){
-
-        // ChromeOptions chromeOptions = new ChromeOptions();
-        // chromeOptions.addArguments("--remote-allow-origins=*");
 
         // System Property for Chrome Driver
         System.setProperty("webdriver.chrome.driver","D:\\Drivers\\chromedriver_112.0.5615.49.exe");
---
+++ src/test/java/org/selenium/guestCheckoutUsingDirectBankTransfer.java
@@ -0,0 +1,79 @@
+package org.selenium;
+
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class guestCheckoutUsingDirectBankTransfer {
+
+    @Test
+    public void Dummytest() throws InterruptedException{
+
+        // System Property for Chrome Driver
+        System.setProperty("webdriver.chrome.driver","D:\\Drivers\\chromedriver_112.0.5615.49.exe");
+        // Instantiate a ChromeDriver class.
+        WebDriver driver=new ChromeDriver();
+
+        // Launch Website
+        driver.get("https://askomdch.com/");
+
+        //Maximize the browser
+        driver.manage().window().maximize();
+
+        // Test
+        driver.findElement(By.xpath("(//li/a[contains(.,'Store')])[1]")).click();
+        driver.findElement(By.xpath("//input[@class='search-field']")).sendKeys("Blue");
+        driver.findElement(By.xpath("//button[@value='Search']")).click();
+        // driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+
+        Assert.assertEquals(
+                  driver.findElement(By.xpath("//h1[contains(@class,'woocommerce-products-header__title page-title')]")).getText(),
+                  "Search results: “Blue”"
+        );
+
+        driver.findElement(By.xpath("//a[@href='?add-to-cart=1215'][contains(.,'Add to cart')]")).click();
+        Thread.sleep(3000);
+        // driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+        driver.findElement(By.xpath("//a[@title='View cart']")).click();
+
+        Assert.assertEquals(driver.findElement(By.xpath("//a[contains(.,'Blue Shoes')]")).getText(),
+                 "Blue Shoes"
+        );
+        Thread.sleep(4000);
+        // driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(4));
+        driver.findElement(By.xpath("//a[contains(@class,'checkout-button button alt wc-forward')]")).click();
+
+        driver.findElement(By.id("billing_first_name")).sendKeys("Test");
+        driver.findElement(By.id("billing_last_name")).sendKeys("Test");
+        driver.findElement(By.id("billing_company")).sendKeys("Testing ORG");
+        driver.findElement(By.xpath("//span[@id='select2-billing_country-container']")).click();
+        driver.findElement(By.xpath("//span/ul/li[contains(.,'Peru')]")).click();
+        driver.findElement(By.id("billing_address_1")).sendKeys("Chosica - Lima");
+        driver.findElement(By.id("billing_address_2")).sendKeys("Perú 2023");
+        driver.findElement(By.id("billing_city")).sendKeys("Lima");
+        driver.findElement(By.xpath("//span[@id='select2-billing_state-container']")).click();
+        driver.findElement(By.xpath("//ul/li[@class='select2-results__option'][contains(.,'Lima')]")).click();
+        driver.findElement(By.id("billing_postcode")).sendKeys("15744");
+        driver.findElement(By.id("billing_phone")).sendKeys("978554490");
+        driver.findElement(By.id("billing_email")).sendKeys("test@gmail.com");
+        driver.findElement(By.id("order_comments")).sendKeys("Orden de compra de prueba");
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("//input[@id='payment_method_cod']")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.id("place_order")).click();
+        Thread.sleep(3000);
+        Assert.assertEquals(driver.findElement(By.xpath("//h1[contains(.,'Checkout')]")).getText(),
+                "Checkout"
+        );
+
+        driver.close();
+        driver.quit();
+
+    }
+
+}
