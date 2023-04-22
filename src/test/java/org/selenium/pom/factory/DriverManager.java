fca546408b382e880e70fd503acfbad2b0803950
Sergio Villagomez Ortega <martin.v.ortega@gmail.com>
2023-04-22 13:09:22 -0500

    Agregando confiruacion de WebDriverManager

--- src/test/java/org/selenium/pom/factory/DriverManager.java
+++ src/test/java/org/selenium/pom/factory/DriverManager.java
@@ -1,5 +1,6 @@
 package org.selenium.pom.factory;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -8,6 +9,15 @@ import java.time.Duration;
 public class DriverManager {
 
     public WebDriver initializeDriver(){
+        WebDriverManager.chromedriver().cachePath("Drivers").setup();
+        WebDriver driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        // driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10)); // Usando Espera implicit
+        return driver;
+
+    }
+
+    public WebDriver initializeDriver2(){
 
         // Instantiate a ChromeDriver class.
         WebDriver driver=new ChromeDriver();
